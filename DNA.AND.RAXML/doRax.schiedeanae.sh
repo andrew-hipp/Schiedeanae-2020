7ec146124a33db67574ab7ed31ac73a936b300e2
Andrew Hipp <andrew-hipp@users.noreply.github.com>
2021-01-02 14:33:32 -0600

    DNA and RAxML upload

---
+++ DNA.AND.RAXML/doRax.schiedeanae.sh
@@ -0,0 +1,4 @@
+raxmlHPC-PTHREADS-SSE3 -f a -T 8 -s concat.schiedeanae.phy -m GTRGAMMA -# 200 -p 12345 -x 12345 -n concat.schiedeanae.tre
+raxmlHPC-PTHREADS-SSE3 -f a -T 8 -s ETS.schiedeanae.phy -m GTRGAMMA -# 200 -p 12345 -x 12345 -n ETS.schiedeanae.tre
+raxmlHPC-PTHREADS-SSE3 -f a -T 8 -s ITS.schiedeanae.phy -m GTRGAMMA -# 200 -p 12345 -x 12345 -n ITS.schiedeanae.tre
+# raxmlHPC-PTHREADS-SSE3 -f a -T 8 -s matK.schiedeanae.phy -m GTRGAMMA -# 200 -p 12345 -x 12345 -n matK.schiedeanae.tre
